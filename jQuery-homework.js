ca141fbbda5d7059c3170b24d861a73b2c39e8a3
shawn huda <shawn.huda@gmail.com>
2017-11-01 17:47:16 -0400

    on click

--- jQuery-homework.js
+++ jQuery-homework.js
@@ -1,32 +1,26 @@
 $(document).ready(function() {
-  $("div.upper").hide(3000);
-  $("div.upper").show(3000);
 
-  $("div.lower").fadeOut(3000);
-  $("div.lower").fadeIn(3000);
+  $("body").click(function() {
+    $("div.upper").hide(3000),
+    $("div.upper").show(3000);
 
-  $("div.middle").slideUp(3000);
-  $("div.middle").slideDown(3000);
+    $("div.lower").fadeOut(3000);
+    $("div.lower").fadeIn(3000);
 
-  $("div.left").animate({
-    width: "30%",
-  }, 3000);
+    $("div.middle").slideUp(3000);
+    $("div.middle").slideDown(3000);
 
-  $("div.left").css("background-color","red");
+    $("div.left").animate({
+      width: "30%",
+    }, 3000);
 
-  $("div.right").animate({
-    width: "0%",
-  }, 3000);
+    $("div.left").css("background-color","red");
+
+    $("div.right").animate({
+      width: "0%",
+    }, 3000);
+
+  });
+});
 
   // FIXME: can you combine two actions without an event trigger?
-
-  //   , function() {
-  //     $(".box").animate({
-  //       width: 100,
-  //       height: 100,
-  //       opacity: 0.5
-  //     }, 2000);
-  //   });
-  // });
-
-});
